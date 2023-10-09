a83abd123d6b8c5e88b83dce03ef1dcf9ec6c553
Saurabh Mandlik <mandliks1996@gmail.com>
2023-10-09 18:04:04 +0530

    Initiating file 3

--- AKS/provider.tf
+++ AKS/provider.tf
@@ -1,12 +1,20 @@
+
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "manduterraformrg" 
-    storage_account_name = "terraformstorageaccn007"
-    container_name       = "terraformcontainer"
-    key                  = "terraform.tfstate"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
   }
-  
+  backend "azurerm" {
+      resource_group_name  = "manduterraformrg"
+      storage_account_name = "terraformstorageaccn007"
+      container_name       = "terraformcontainer"
+      key                  = "terraform.tfstate"
+  }
+
 }
+
 provider "azurerm" {
    subscription_id      = "896c718b-3691-41d7-81ed-6587d464a0c9"
     client_id            = var.spn-client-id     // This is the application id
