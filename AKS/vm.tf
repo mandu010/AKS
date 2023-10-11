176cff7298da0baa02a6808b28e6923ade7a8ea7
Saurabh Mandlik <mandliks1996@gmail.com>
2023-10-11 17:33:43 +0530

    Associated Public IP with NIC

--- AKS/vm.tf
+++ AKS/vm.tf
@@ -24,6 +24,7 @@ resource "azurerm_network_interface" "jenkinsNic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.aks_subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.jenkinsPublic.id
   }
 }
 
