3f65c653d62fa193affd01158c8a9b7faf64badf
Saurabh Mandlik <mandliks1996@gmail.com>
2023-10-12 14:04:07 +0530

    Shell changes

--- AKS/Setup.bash
+++ AKS/Setup.bash
@@ -1,24 +1,41 @@
+#### Jenkins Maven Kubectl Helm Docker Setup for Ubuntu ###
+
+
 ### Downloading and Installing Kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 mv kubectl /usr/bin/
 chmod +x /usr/bin/kubectl
+echo "Run command on AZ: az aks get-credentials --resource-group=akscluster-rg --name=akscluster"
 
 ### Downloading and Installing Helm
 wget https://get.helm.sh/helm-v3.13.0-linux-amd64.tar.gz
 tar -zxvf helm-v3.13.0-linux-amd64.tar.gz
 mv linux-amd64/helm /usr/bin/
 chmod +x /usr/bin/helm
+helm repo add stable https://charts.helm.sh/stable ### Adding stable helm repo 
 
 ### Cleanup
 rm -rf *.gz *amd*
 
+### Installing Java
+echo "Installing & Configuring Java now"
+wget https://download.java.net/java/GA/jdk13.0.1/cec27d702aa74d5a8630c65ae61e4305/9/GPL/openjdk-13.0.1_linux-x64_bin.tar.gz
+tar -xvf openjdk-13.0.1_linux-x64_bin.tar.gz
+mv jdk-13.0.1 /opt/
+JAVA_HOME='/opt/jdk-13.0.1'
+PATH="$JAVA_HOME/bin:$PATH"
+export PATH
+source ~/.bashrc
+java -version
+
 ### Installing Jenkins
-sudo apt install openjdk-11-jre -y
+echo "Installing Jenkins now"
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo 'deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/' | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update && sudo apt-get install jenkins -y
 sudo service jenkins restart
 echo "Jenkins Initial Password is:" && sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+echo "Add Jenkins profile to Sudoers file"
 
 ### Downloading and Installing Maven
 rm -rf *.gz *amd*
