750117bb7c6f635c90cfef5c8f348a9769061804
Saurabh Mandlik <mandliks1996@gmail.com>
2023-10-15 14:40:39 +0530

    Removing and adding different java app

--- AKS/Setup.sh
+++ AKS/Setup.sh
@@ -36,7 +36,7 @@ java -version
 echo "Installing Jenkins now"
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo 'deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/' | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update && sudo apt-get install jenkins -y
+sudo apt-get install jenkins -y
 sudo service jenkins restart
 echo "Jenkins Initial Password is:" && sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 echo "Add Jenkins profile to Sudoers file"
