96a4e6f21353f4b9d804ab96deccf0b787ad4968
Saurabh Mandlik <mandliks1996@gmail.com>
2023-10-16 10:51:32 +0530

    K8s delete

--- AKS/main.tf
+++ AKS/main.tf
@@ -48,39 +48,39 @@ resource "azurerm_resource_group" "aks_rg" {
   location = var.azure_region
 }
 
-resource "azurerm_kubernetes_cluster" "aks_cluster" {
-  name                = var.cluster_name
-  location            = azurerm_resource_group.aks_rg.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
-  dns_prefix          = var.dns_name
+# resource "azurerm_kubernetes_cluster" "aks_cluster" {
+#   name                = var.cluster_name
+#   location            = azurerm_resource_group.aks_rg.location
+#   resource_group_name = azurerm_resource_group.aks_rg.name
+#   dns_prefix          = var.dns_name
 
 
-  default_node_pool {
-    name            = var.agent_pools.name
-    node_count      = var.agent_pools.count
-    vm_size         = var.agent_pools.vm_size
-    os_disk_size_gb = var.agent_pools.os_disk_size_gb
-  }
+#   default_node_pool {
+#     name            = var.agent_pools.name
+#     node_count      = var.agent_pools.count
+#     vm_size         = var.agent_pools.vm_size
+#     os_disk_size_gb = var.agent_pools.os_disk_size_gb
+#   }
   
 
-  linux_profile {
-    admin_username = var.admin_username
-    ssh_key {
-      key_data = data.azurerm_key_vault_secret.ssh_public_key.value
-    }
-  }
+#   linux_profile {
+#     admin_username = var.admin_username
+#     ssh_key {
+#       key_data = data.azurerm_key_vault_secret.ssh_public_key.value
+#     }
+#   }
 
-  # role_based_access_control {
-  #   enabled = true
-  # }
+#   # role_based_access_control {
+#   #   enabled = true
+#   # }
 
 
-  service_principal {
-    client_id     = data.azurerm_key_vault_secret.spn_id.value
-    client_secret = data.azurerm_key_vault_secret.spn_secret.value
-  }
+#   service_principal {
+#     client_id     = data.azurerm_key_vault_secret.spn_id.value
+#     client_secret = data.azurerm_key_vault_secret.spn_secret.value
+#   }
 
-  tags = {
-    Environment = "Demo"
-  }
-}
+#   tags = {
+#     Environment = "Demo"
+#   }
+# }
