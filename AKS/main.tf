21fc32d9d2f7a69f42e7af7353ed211a0ffbbacc
Saurabh Mandlik <mandliks1996@gmail.com>
2023-10-23 23:02:58 +0530

    123

--- AKS/main.tf
+++ AKS/main.tf
@@ -20,33 +20,33 @@ data "azurerm_key_vault_secret" "spn_secret" {
   key_vault_id = data.azurerm_key_vault.azure_vault.id
 }
 
-# data "azuread_service_principal" "aks_principal" {
-#   application_id = data.azurerm_key_vault_secret.spn_id.value
-# }
+data "azuread_service_principal" "aks_principal" {
+  application_id = data.azurerm_key_vault_secret.spn_id.value
+}
 
 /*
   Creating Resources
 */
 
 
-# resource "azurerm_virtual_network" "aks_vnet" {
-#   name                = var.aks_vnet_name
-#   resource_group_name = azurerm_resource_group.aks_rg.name
-#   location            = azurerm_resource_group.aks_rg.location
-#   address_space       = var.vnetcidr
-# } 
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = var.aks_vnet_name
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  location            = azurerm_resource_group.aks_rg.location
+  address_space       = var.vnetcidr
+} 
 
-# resource "azurerm_subnet" "aks_subnet" {
-#   name                 = "aks_subnet"
-#   resource_group_name  = azurerm_resource_group.aks_rg.name
-#   virtual_network_name = azurerm_virtual_network.aks_vnet.name
-#   address_prefixes       = var.subnetcidr
-# }
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks_subnet"
+  resource_group_name  = azurerm_resource_group.aks_rg.name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes       = var.subnetcidr
+}
 
-# resource "azurerm_resource_group" "aks_rg" {
-#   name     = var.resource_group
-#   location = var.azure_region
-# }
+resource "azurerm_resource_group" "aks_rg" {
+  name     = var.resource_group
+  location = var.azure_region
+}
 
 # resource "azurerm_kubernetes_cluster" "aks_cluster" {
 #   name                = var.cluster_name
